59ee5bf85e91dabd046ed1195b617b294f508369
sameer singh <sameer@Sameers-MacBook-Pro.local>
2026-03-04 02:07:49 +0530

    added one basic urlencoded line

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ import { Url } from "./src/models/url.model";
 export function createApp() {
     const app = express();
     app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
     const repository = new InMemoryUrlRepository();
     const cache = new InMemoryCache<Url>(5 * 60 * 1000); // 5 min TTL
